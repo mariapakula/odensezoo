01af06d12a772c1862e2484a4865613b359418c3
mariapakula <48321478+mariapakula@users.noreply.github.com>
2020-05-23 15:24:45 +0200

    rettelser i database

--- databaseconnection.php
+++ databaseconnection.php
@@ -3,6 +3,7 @@ $servername = "mariapakula.dk.mysql";
 $username = "mariapakula_dk";
 $password = "HUCyVKkEtErSL2ADxQWKBYgp";
 $dbname = "mariapakula_dk";
+$pass = sha1($_POST[password]);
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,18 +12,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO odensezoo (firstname, lastname, email, password)
-VALUES ('$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[password]')";
+$sql = "INSERT INTO odensezoo (firstname, lastname, kortid, password)
+VALUES ('$_POST[firstname]','$_POST[lastname]','$_POST[email]','$pass')";
 
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    header("Location: aarskortoversigt.html?sign=ok"); /* Redirect browser */
+    exit();
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 
 $conn->close();
-header("Location: aarskortoversigt.html?sign=ok"); /* Redirect browser */
-exit();
 ?>
